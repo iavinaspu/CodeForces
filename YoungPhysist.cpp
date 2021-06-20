8b9d480a2529e939bbdeb260c21116a9630c384f
AVINASH PUJARI <pujariavinash97@gmail.com>
2021-06-20 18:32:15 +0530

    Young Physicist

---
+++ YoungPhysist.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int n, x, y, z, xsum(0), ysum(0), zsum(0);
+cin >> n;
+
+while (n--)
+{
+    cin >> x >> y >> z;
+    xsum += x;
+    ysum += y;
+    zsum += z;
+}
+
+if (xsum == 0 && ysum == 0 && zsum == 0)
+{
+    cout << "YES" << endl;
+}
+else
+{
+    cout << "NO" << endl;
+}
+return 0;
+}
